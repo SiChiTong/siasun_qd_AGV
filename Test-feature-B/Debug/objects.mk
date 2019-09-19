01ef977a016fd6c635e6ed84b4e5698359142833
maxiao <18322693218@163.com>
2019-09-19 14:29:58 +0800

    9.19

--- Test-feature-B/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lpthread -l/home/siasunhebo/workspace/sensorTest/UART/UART.h
-
--- Test-feature-B/include/include.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by siasunhebo on 8/25/19.
-//
-
-#ifndef _INCLUDE_H
-#define _INCLUDE_H
-
-#endif //IMUTEST_INCLUDE_H
